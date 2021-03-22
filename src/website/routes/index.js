50db6e650c9d0d260fdbeb21fb22deee3f0b7839
Pedrao <tellayprivadobuilds@gmail.com>
2021-03-22 21:33:06 +0000

    first commit

---
+++ src/website/routes/index.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const moment = require('moment');
+
+router.get('/', async (req, res) => {
+    res.render('landing_page.ejs');
+});
+
+router.get('/io', async (req, res) => {
+    res.render('io.ejs');
+});
+
+router.post('/io', async (req, res) => {
+    const { ID } = req.body;
+    const User = await req.bot.users.fetch(ID).catch(err => res.redirect('/'));
+    res.render('io.ejs', { user: User, createdAt: moment(User.createdTimestamp).format('l') });
+})
+
+module.exports = router;
